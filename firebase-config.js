9e85ba48e2fee7ad3c646fb48ff230478e873d3f
nagi <mx.gin.xo@gmail.com>
2026-04-30 17:08:32 +0900

    fix: expose firebase config for GitHub Pages (web API keys are public identifiers)

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChBUc52-GfMk49hQBbcHFK8xW0mjtEFLE",
+  authDomain: "demian-chat.firebaseapp.com",
+  databaseURL: "https://demian-chat-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "demian-chat",
+  storageBucket: "demian-chat.firebasestorage.app",
+  messagingSenderId: "217240835796",
+  appId: "1:217240835796:web:3375708a2924adda6a0d41"
+};
